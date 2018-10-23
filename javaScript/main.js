b9282b3486b0dcdf1fa17c61f5a87f06296a571a
SamJose123 <samantha.joseph27@gmail.com>
2018-10-23 14:20:12 +0200

    exercise

---
+++ javaScript/main.js
@@ -0,0 +1,21 @@
+var x=5;
+console.log(x);
+
+// variable type string
+var name ='Samantha Joseph';
+
+// console shows name
+// concat (strings with variable)
+console.log ('My name is '+ name);
+
+var name = 'Sally';
+var count = 3;
+var pets = 'dogs'
+var country = 'America';
+console.log('My name is '+ name + ' and own ' +  count  +  pets + ' and I live in '+ country )
+
+
+
+
+
+ 
