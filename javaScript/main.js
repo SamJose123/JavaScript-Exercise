b16ebcfa592c40d625b7edb589d60af35041b4d2
SamJose123 <samantha.joseph27@gmail.com>
2018-10-24 11:46:06 +0200

    creating a button in js

--- javaScript/main.js
+++ javaScript/main.js
@@ -10,9 +10,38 @@ console.log ('My name is '+ name);
 
 var name = 'Sally';
 var count = 3;
-var pets = 'dogs'
+var pets = 'dogs';
 var country = 'America';
-console.log('My name is '+ name + ' and own ' +  count  +  pets + ' and I live in '+ country )
+console.log('My name is '+ name + ' and own ' +  count  +  pets + ' and I live in '+ country );
+
+
+// declare a variable
+// var myName = 'Samantha';
+// var sayHello = 'Hi my name is ' + myName;
+
+
+
+// basic function and onclick event
+document.getElementById('btn').onclick = function () {
+
+    // grab input value
+    var myName = document.getElementById('name').value;
+    
+    if (myName) {
+
+    // create my sentence
+    var sayHello = 'Hi my name is ' + myName;
+
+    // targeting an element is HTML
+    document.getElementById('hello').innerHTML = sayHello;
+
+    } else {
+        alert('You forgot to enter your name fool!')
+    }
+}
+
+
+
 
 
 
